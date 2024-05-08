7c626b1494e8ed18755c93c6fe970f3108a9158e
= <=>
2024-05-09 01:25:57 +0900

    ✨ FEAT. 회원가입 API 작성
    FEAT. 회원가입 API 작성

---
+++ src/main/java/com/homepage/likelion/test/domain/User.java
@@ -0,0 +1,32 @@
+package com.homepage.likelion.test.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "USER")
+@Getter
+// Entity에는 @Setter를 사용하지 않는다.
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+}
