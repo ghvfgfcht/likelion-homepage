5d554e3e653b2892ad61f2a23fa172d9ab0f56e7
= <=>
2024-05-08 17:57:11 +0900

    ✨ 간단한 테스트 컨트롤러 작성
    :sparkles: FEAT. 간단한 테스트 컨트롤러 작성

---
+++ src/main/java/com/homepage/likelion/test/SimpleTestController.java
@@ -0,0 +1,27 @@
+package com.homepage.likelion.test;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/simpleText")
+public class SimpleTestController {
+
+    // GET http://localhost:8080/api/simpleText/success
+    @GetMapping("/success")
+    public String simpleTextSuccess(){
+        return "안녕";
+    }
+
+    // GET http://localhost:8080/api/simpleText/fail
+    @GetMapping("/fail")
+    public ResponseEntity<String> simpleTextFail(){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("실패");
+    }
+
+
+
+}
